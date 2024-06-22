9ad9cbf524417d02428a665e0c7953310b31e801
yamdeng <yamdeng@gmail.com>
2024-06-22 19:09:03 +0900

    project init

---
+++ src/main/java/com/yamdeng/learn/spring/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.yamdeng.learn.spring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class TestController {
+
+    @GetMapping("/api/test")
+    public String getMethodName() {
+        return "yamdeng";
+    }
+
+}
