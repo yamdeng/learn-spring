232f44eda9d1c4e5934e50cf0f04fbe3730c7042
yamdeng <yamdeng@gmail.com>
2024-06-28 16:27:17 +0900

    alias 최신화

---
+++ docs/alias.cmd
@@ -0,0 +1,13 @@
+@echo off
+doskey ls=dir
+doskey gb=git branch
+doskey gbr=git branch -r
+doskey gf=git fetch
+doskey gp=git push
+doskey cde=code .
+doskey yd=yarn dev
+doskey yb=yarn build
+doskey gbt=gradlew bootRun
+doskey ex=explorer .
+doskey ch=cd C:\Project\korean
+@echo on
